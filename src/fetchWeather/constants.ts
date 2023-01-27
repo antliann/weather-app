f5162ea36768ac6b52482dc85cf9baf18dd4edac
Anton Liannoi <antonli@wix.com>
2023-01-28 00:19:33 +0100

    var name fix

--- src/fetchWeather/constants.ts
+++ src/fetchWeather/constants.ts
@@ -2,5 +2,5 @@ export const URL = 'https://api.openweathermap.org/data/2.5/forecast';
 
 export const DEFAULT_REQUEST_PARAMS = {
   units: 'metric',
-  appid: process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY, // this is INSECURE and would not be used in a real-world project
+  appid: process.env.REACT_APP_OPENWEATHER_API_KEY, // this is INSECURE and would not be used in a real-world project
 };
