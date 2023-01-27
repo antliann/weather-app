8be5bfb64c0b981c38a4a3e529427d6ed331ba13
Anton Liannoi <antonli@wix.com>
2023-01-28 00:19:21 +0100

    weekdays added

--- src/constants.ts
+++ src/constants.ts
@@ -8,4 +8,5 @@ export interface WeatherData {
   temperature: number;
   description: string;
   icon: string;
+  weekDay: number;
 }
--- src/fetchWeather/extractWeatherData.ts
+++ src/fetchWeather/extractWeatherData.ts
@@ -19,9 +19,9 @@ interface WeatherResponseData {
   };
 }
 
-const getTimezoneDate = (timestamp: number, offsetInSeconds: number) => {
+const getTimezoneDay = (timestamp: number, offsetInSeconds: number) => {
   const timestampWithOffset = timestamp + offsetInSeconds * 1000;
-  return new Date(timestampWithOffset).getUTCDate();
+  return new Date(timestampWithOffset).getUTCDay();
 };
 
 export const extractWeatherData = (
@@ -30,18 +30,21 @@ export const extractWeatherData = (
   const closestRecord = data.list[0];
 
   const { timezone } = data.city;
-  const currentTimezoneDate = getTimezoneDate(Date.now(), timezone);
+  const currentTimezoneDay = getTimezoneDay(Date.now(), timezone);
 
-  const nextDaysForecast = data.list.filter(record => {
-    const recordDate = getTimezoneDate(record.dt * 1000, timezone);
-    return (
-      record.dt_txt.endsWith('12:00:00') && recordDate !== currentTimezoneDate
-    );
-  });
+  const nextDaysForecast = data.list
+    .filter(record => {
+      const recordDay = getTimezoneDay(record.dt * 1000, timezone);
+      return (
+        record.dt_txt.endsWith('12:00:00') && recordDay !== currentTimezoneDay
+      );
+    })
+    .slice(0, 4);
 
-  return [closestRecord, ...nextDaysForecast].map(record => ({
+  return [closestRecord, ...nextDaysForecast].map((record, index) => ({
     temperature: record.main.temp,
     description: record.weather[0].main,
     icon: record.weather[0].icon,
+    weekDay: currentTimezoneDay + index,
   }));
 };
