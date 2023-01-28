561bc05a6b073216d6d12875a6c453087fc8e393
Anton Liannoi <antonli@wix.com>
2023-01-28 17:03:38 +0100

    new request url & config

--- src/fetchWeather/constants.ts
+++ src/fetchWeather/constants.ts
@@ -1,6 +1,18 @@
-export const URL = 'https://api.openweathermap.org/data/2.5/forecast';
+import { City } from '../constants';
+
+export const URL = 'https://api.openweathermap.org/data/3.0/onecall';
 
 export const DEFAULT_REQUEST_PARAMS = {
   units: 'metric',
+  exclude: 'current,minutely,hourly,alerts',
   appid: process.env.REACT_APP_OPENWEATHER_API_KEY, // this is INSECURE and would not be used in a real-world project
+};
+
+export const MAP_CITY_TO_COORDINATES: Record<
+  City,
+  [latitude: number, longitude: number]
+> = {
+  [City.Ottawa]: [45.4112, -75.6981],
+  [City.Moscow]: [55.7522, 37.6156],
+  [City.Tokyo]: [35.6895, 139.6917],
 };
--- src/fetchWeather/fetchWeather.ts
+++ src/fetchWeather/fetchWeather.ts
@@ -1,12 +1,20 @@
 import axios from 'axios';
 import { type WeatherData, type City } from '../constants';
-import { DEFAULT_REQUEST_PARAMS, URL } from './constants';
+import {
+  DEFAULT_REQUEST_PARAMS,
+  MAP_CITY_TO_COORDINATES,
+  URL,
+} from './constants';
 import { extractWeatherData } from './extractWeatherData';
 
 export const fetchWeather = (city: City): Promise<WeatherData[]> => {
+  const [latitude, longitude] = MAP_CITY_TO_COORDINATES[city];
+
   const requestParams = {
     ...DEFAULT_REQUEST_PARAMS,
     q: city,
+    lat: latitude,
+    lon: longitude,
   };
 
   return axios
